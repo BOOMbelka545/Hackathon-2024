ee186434fd84f530fb84f1d3cc2bf32725c3974f
Leonid Zamoreev <boombelka545@gmail.com>
2024-04-19 22:53:22 +0300

    100% full version of last commit

---
+++ backend/internal/config/config.go
@@ -0,0 +1,51 @@
+package config
+
+import (
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/labstack/gommon/log"
+)
+
+type (
+	Config struct {
+		Env         string `yaml:"env" env-default:"local"`
+		StoragePath string `yaml:"storage_path" env-required:"true"`
+		HTTPServer  `yaml:"http_server"`
+		DB          `yaml:"db"`
+	}
+
+	HTTPServer struct {
+		Address     string        `yaml:"address" env-default:"localhost:8080"`
+		TimeOut     time.Duration `yaml:"timeout" env-default:"4s"`
+		IdleTimeOut time.Duration `yaml:"idle_timeout" env-default:"60s"`
+	}
+
+	DB struct {
+		Address  string `yaml:"address" env-default:"localhost:5432"`
+		User     string `yaml:"user" env-required:"true"`
+		Password string `yaml:"password" env-required:"true"`
+		NameDB   string `yaml:"name_db" env-required:"true"`
+	}
+)
+
+func MustLoad() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH is no set")
+	}
+
+	// check if file exist
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file is not exist: %s", configPath)
+	}
+
+	var cfg Config
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("cannot read config: %s", err)
+	}
+
+	return &cfg
+}
